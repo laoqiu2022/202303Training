441c8870bb21179f4b18be10ee640a863279e288
laoqiu <laoqiu2022@outlook.com>
2023-02-28 23:38:09 +0900

    初回更新

---
+++ 練習1/FolderFileRename.ps1
@@ -0,0 +1,29 @@
+$inputForlder = ""
+
+if(![string]::IsNullorEmpty($args[0])){
+    if(Test-Path $args[0]){
+        $inputForlder = $args[0]
+    }
+    else{
+        Write-Host("Forlder not exist.") -BackgroundColor Blue;
+        return
+    }
+}
+
+if($inputForlder -ne ""){
+    Get-ChildItem -Path $inputForlder | ForEach-Object -Process{
+        if($_ -is [System.IO.FileInfo])
+        {
+            Write-Host($_.CreationTime);
+            Write-Host($_.FullName);
+
+            Rename-Item -Path $_.FullName `
+            -NewName ($_.LastWriteTime.ToString('yyyyMMdd-HHmmss.fff') + '-' + $_.BaseName + $_.Extension)
+        }
+    }
+}
+else{
+    Write-Host("Forlder is empty.") -BackgroundColor Blue;
+}
+
+#Write-Host($_.FullName);
